b2c23489824738b892b7f5e9e79226feceba98d5
Adem Güney <ex_ademg@koczer.com>
2022-10-22 15:11:36 +0300

    Blogi.Dashborad deleted. Because using area.

---
+++ BlogiBlogArchitecture/src/blogiBlog/Presentation/Blogi.WebUI/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blogi.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+    }
+}
--- BlogiBlogArchitecture/src/blogiBlog/Presentation/Blogi.WebUI/Program.cs
+++ BlogiBlogArchitecture/src/blogiBlog/Presentation/Blogi.WebUI/Program.cs
@@ -21,4 +21,12 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+});
+
 app.Run();
