6453f1e58c1e270cc642e5b79ea993198966deb4
Adem Güney <ex_ademg@koczer.com>
2022-11-01 15:44:52 +0300

    Tag CRUD controller and view created.

---
+++ BlogiBlogArchitecture/src/blogiBlog/Presentation/Blogi.Dashboard/Controllers/TagController.cs
@@ -0,0 +1,84 @@
+﻿using Blogi.Application.Features.Tags.Commands.Create;
+using Blogi.Application.Features.Tags.Commands.Delete;
+using Blogi.Application.Features.Tags.Commands.Update;
+using Blogi.Application.Features.Tags.Queries.Get;
+using Blogi.Application.Features.Tags.Queries.GetList;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blogi.Dashboard.Controllers
+{
+    public class TagController : BaseController
+    {
+        [HttpGet]
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CreateTagCommand input)
+        {
+            var result = await Mediator.Send(input);
+            if (result.Success)
+            {
+                NotifySuccess(result.Message);
+                return RedirectToAction("Home", "Tag");
+            }
+            else
+            {
+                NotifyError(result.Errors);
+                return View(input);
+            }
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> Delete(DeleteTagCommand input)
+        {
+            var result = await Mediator.Send(input);
+            return Json(new { data = result });
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(GetTagQuery input)
+        {
+            var result = await Mediator.Send(input);
+            if (result.Success)
+                return View(result.Data);
+            else
+            {
+                NotifyError(result.Errors);
+                return RedirectToAction("Home", "Tag");
+            }
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(UpdateTagCommand input)
+        {
+            var result = await Mediator.Send(input);
+            if (result.Success)
+            {
+                NotifySuccess(result.Message);
+                return RedirectToAction("Home", "Tag");
+            }
+            else
+            {
+                NotifyError(result.Errors);
+                return View(result.Data);
+            }
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> DataTable()
+        {
+            var result = await Mediator.Send(new GetListTagQuery());
+            return Json(new { data = result.Data });
+        }
+    }
+}
